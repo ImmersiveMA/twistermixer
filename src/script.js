a050f683e73a51d04eca2bb6c93ccb2058710f80
Jaap <jareerink@gmail.com>
2022-04-20 12:51:36 +0200

    first color slider

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 const sound = document.getElementById("sound");
 const tempLights = document.getElementById("tempLights");
+const color1 = document.getElementById('color1');
 
 // Sound list:
 const calm = new Audio('./audio/Sweet.mp3');
@@ -59,3 +60,32 @@ function request(url) {
     http.open("GET", url);
     http.send();
 }
+
+let green;
+let blue;
+let red;
+
+color1.addEventListener('input', () => {
+    
+
+    if(color1.value > 0 && color1.value < 255) {
+        red = 255 - color1.value;
+        green = color1.value;
+        blue = 0;
+    }
+    
+    if(color1.value > 255 && color1.value < 510) {
+        green = 255 - (color1.value-255)
+        blue = color1.value - 255;
+        red = 0;
+    }
+    
+    if(color1.value > 510 && color1.value < 765) {
+        blue = 255 - (color1.value - 510);
+        red = color1.value - 510;
+        green = 0;
+    }
+    console.log("green: ", green);
+    console.log("red: ", red);
+    console.log('blue: ', blue);
+})
