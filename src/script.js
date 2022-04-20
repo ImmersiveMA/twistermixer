80e6f3e2bf39cff10e4d132a9d6f268fc261bbee
Jaap <jareerink@gmail.com>
2022-04-20 16:42:52 +0200

    light edits

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const sound = document.getElementById("sound");
 const tempLights = document.getElementById("tempLights");
 const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+
 
 // Sound list:
 const calm = new Audio('./audio/Sweet.mp3');
@@ -61,59 +63,108 @@ function request(url) {
     http.send();
 }
 
-let green;
-let blue;
-let red;
 
-const showcase = document.getElementById('showcase');
+
+let delay = 100;
+let cantSend = true;
+let cantSend2 = true;
+
+color2.addEventListener('input', () => {
+    
+    color = getColor(color2.value);
+    showcase.style.backgroundColor = `rgb( ${color.red}, ${color.green}, ${color.blue})`;
+
+    if(!cantSend2) return;
+    cantSend2 = false;
+
+    request(`http://192.168.137.15/color?r=${color.red}&g=${color.green}&b=${color.blue}`);
+
+    setTimeout(() => {
+        cantSend2 = true;
+    }, delay);
+})
 
 color1.addEventListener('input', () => {
     
+    color = getColor(color1.value);
+
+
+    if(!cantSend) return;
+    cantSend = false;
+    request(`http://192.168.137.168/color?r=${color.red}&g=${color.green}&b=${color.blue}`);
+    request(`http://192.168.137.190/color?r=${color.red}&g=${color.green}&b=${color.blue}`);
+    request(`http://192.168.137.37/color?r=${color.red}&g=${color.green}&b=${color.blue}`);
+
+    console.log(color1.value, `r=${color.red},g=${color.green},b=${color.blue}`)
+
+    setTimeout(() => {
+        cantSend = true;
+    }, delay);
+
+});
+
+function getColor (color) {
+    let green;
+    let blue;
+    let red;
+    
+    
+    if(color < 15) {
+        blue = 0;
+        green = 0;
+        red = 255;
+    }
+    
     //rood -> geel
-    if(color1.value > 0 && color1.value < 127) {
+    if(color > 15 && color < 127) {
         blue = 0;
         red = 255;
-        green = 2*color1.value;
+        green = 2*color;
     }
     
     //geel -> groen
-    if(color1.value > 127 && color1.value < 255) {
+    if(color > 127 && color < 255) {
         blue = 0;
         green = 255;
-        red = 255 - (2*(color1.value - 127));
+        red = 255 - (2*(color - 127));
     }
 
     //groen -> cyaan
-    if(color1.value >255 && color1.value < 382) {
+    if(color >255 && color < 382) {
         red = 0
         green = 255;
-        blue = 2* (color1.value - 255);
+        blue = 2* (color - 255);
     }
 
     //cyaan -> blauw
-    if(color1.value > 382 && color1.value < 510) {
+    if(color > 382 && color < 510) {
         red = 0;
         blue = 255;
-        green = 255 - (2*(color1.value - 382))
+        green = 255 - (2*(color - 382))
     }
     
     //blauw -> paars
-    if(color1.value > 510 && color1.value < 637) {
+    if(color > 510 && color < 637) {
         green = 0;
         blue = 255;
-        red = 2* (color1.value - 510);
+        red = 2* (color - 510);
     }
 
-    if(color1.value > 637 && color1.value < 765) {
+    if(color > 637 && color < 750) {
         green = 0;
         red = 255;
-        blue = 255 - (2*(color1.value - 637));
+        blue = 255 - (2*(color - 638));
     }
 
-    showcase.style.backgroundColor = `rgb( ${red}, ${green}, ${blue})`;
+    if(color > 750) {
+        green = 0;
+        blue = 0;
+        red = 255;
+    }
 
-    console.log(color1.value);
-    console.log("green: ", green);
-    console.log("red: ", red);
-    console.log('blue: ', blue);
-})
+    colorobj = {};
+    colorobj.red = red;
+    colorobj.green = green;
+    colorobj.blue = blue;
+    return colorobj;
+}
