bc73f2665d597989d69a22cffb8554d33a0fb07e
Jaap <jareerink@gmail.com>
2022-04-20 13:32:15 +0200

    color slide finish

--- src/script.js
+++ src/script.js
@@ -65,26 +65,54 @@ let green;
 let blue;
 let red;
 
+const showcase = document.getElementById('showcase');
+
 color1.addEventListener('input', () => {
     
-
-    if(color1.value > 0 && color1.value < 255) {
-        red = 255 - color1.value;
-        green = color1.value;
+    //rood -> geel
+    if(color1.value > 0 && color1.value < 127) {
         blue = 0;
+        red = 255;
+        green = 2*color1.value;
     }
     
-    if(color1.value > 255 && color1.value < 510) {
-        green = 255 - (color1.value-255)
-        blue = color1.value - 255;
+    //geel -> groen
+    if(color1.value > 127 && color1.value < 255) {
+        blue = 0;
+        green = 255;
+        red = 255 - (2*(color1.value - 127));
+    }
+
+    //groen -> cyaan
+    if(color1.value >255 && color1.value < 382) {
+        red = 0
+        green = 255;
+        blue = 2* (color1.value - 255);
+    }
+
+    //cyaan -> blauw
+    if(color1.value > 382 && color1.value < 510) {
         red = 0;
+        blue = 255;
+        green = 255 - (2*(color1.value - 382))
     }
     
-    if(color1.value > 510 && color1.value < 765) {
-        blue = 255 - (color1.value - 510);
-        red = color1.value - 510;
+    //blauw -> paars
+    if(color1.value > 510 && color1.value < 637) {
         green = 0;
+        blue = 255;
+        red = 2* (color1.value - 510);
     }
+
+    if(color1.value > 637 && color1.value < 765) {
+        green = 0;
+        red = 255;
+        blue = 255 - (2*(color1.value - 637));
+    }
+
+    showcase.style.backgroundColor = `rgb( ${red}, ${green}, ${blue})`;
+
+    console.log(color1.value);
     console.log("green: ", green);
     console.log("red: ", red);
     console.log('blue: ', blue);
